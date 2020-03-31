bcf5db92a7008e80e069efabac20c6a4751ec100
Tamás Visy <tamas.visy@gmail.com>
2020-03-31 22:53:56 +0200

    Test 5.3.7, Test 5.3.6, added short description

--- Code/Skeleton/src/hu/mudm/icefield/Main.java
+++ Code/Skeleton/src/hu/mudm/icefield/Main.java
@@ -1,8 +1,11 @@
 package hu.mudm.icefield;
 
 import hu.mudm.icefield.model.action.Action;
+import hu.mudm.icefield.model.action.BuildRocketAction;
 import hu.mudm.icefield.model.action.MoveAction;
+import hu.mudm.icefield.model.action.ShovelAction;
 import hu.mudm.icefield.model.field.Hole;
+import hu.mudm.icefield.model.field.InstableIceFloat;
 import hu.mudm.icefield.model.field.StableIceFloat;
 import hu.mudm.icefield.model.item.Rope;
 import hu.mudm.icefield.model.player.Eskimo;
@@ -15,15 +18,21 @@ class Main {
 
         Test_5_3_13();
 
+        Test_5_3_15();
+
+        Test_5_3_7();
+
+        Test_5_3_6();
+
         //System.out.println("Hello");
     }
 
-    static void Test_5_3_1() { //Test for 5.3.1
+    static void Test_5_3_1() { //Test for 5.3.1 - Player steps on stable icefloat
         Action ma = new MoveAction(new Eskimo(new StableIceFloat()), new StableIceFloat());
         ma.performAction();
     }
 
-    static void Test_5_3_13() {  //Test for 5.3.13
+    static void Test_5_3_13() {  //Test for 5.3.13 - Player gets rescued
         StableIceFloat one=new StableIceFloat();
         Hole two = new Hole();
         StableIceFloat three=new StableIceFloat();
@@ -36,8 +45,20 @@ class Main {
         ma_13.performAction();
     }
 
-    static void Test_5_3_15() {  //Test for 5.3.15
+    static void Test_5_3_15() {  //Test for 5.3.15 - Game won
         Game g = new Game();
         g.win();
+    }
+
+    static void Test_5_3_7() { //Test for 5.3.7 - Player builds rocket
+        Eskimo e = new Eskimo(new StableIceFloat());
+        BuildRocketAction bra = new BuildRocketAction(e);
+        bra.performAction();
+    }
+
+    static void Test_5_3_6() { //Test for 5.3.6 - Player shovels snow
+        Researcher r = new Researcher(new InstableIceFloat());
+        ShovelAction s = new ShovelAction(r);
+        s.performAction();
     }
 }
