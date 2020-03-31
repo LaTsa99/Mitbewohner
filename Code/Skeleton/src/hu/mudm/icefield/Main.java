5f7677a8595436fb3c9a1557a5b93cc7b8dd88b6
Tamás Visy <tamas.visy@gmail.com>
2020-03-31 22:22:44 +0200

    Tesztek kiszervezve

--- Code/Skeleton/src/hu/mudm/icefield/Main.java
+++ Code/Skeleton/src/hu/mudm/icefield/Main.java
@@ -4,17 +4,26 @@ import hu.mudm.icefield.model.action.Action;
 import hu.mudm.icefield.model.action.MoveAction;
 import hu.mudm.icefield.model.field.Hole;
 import hu.mudm.icefield.model.field.StableIceFloat;
-import hu.mudm.icefield.model.player.Eskimo;
 import hu.mudm.icefield.model.item.Rope;
+import hu.mudm.icefield.model.player.Eskimo;
 import hu.mudm.icefield.model.player.Researcher;
 
 class Main {
     public static void main(String[] args){
-        //Test for 5.3.1
+
+        Test_5_3_1();
+
+        Test_5_3_13();
+
+        //System.out.println("Hello");
+    }
+
+    static void Test_5_3_1() { //Test for 5.3.1
         Action ma = new MoveAction(new Eskimo(new StableIceFloat()), new StableIceFloat());
         ma.performAction();
+    }
 
-        //Test for 5.3.13
+    static void Test_5_3_13() {  //Test for 5.3.13
         StableIceFloat one=new StableIceFloat();
         Hole two = new Hole();
         StableIceFloat three=new StableIceFloat();
@@ -25,6 +34,5 @@ class Main {
 
         Action ma_13 = new MoveAction(r, two);
         ma_13.performAction();
-        //System.out.println("Hello");
     }
 }
