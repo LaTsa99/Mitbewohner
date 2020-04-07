72d1e9d804b73fed1898cf2e55a1188c5b5f1a1f
Tamás Visy <tamas.visy@gmail.com>
2020-04-07 14:44:22 +0200

    Added BuildTentAction

---
+++ Code/Prototype/src/hu/mudm/icefield/model/action/BuildTentAction.java
@@ -0,0 +1,24 @@
+package hu.mudm.icefield.model.action;
+
+import hu.mudm.icefield.model.field.IceFloat;
+import hu.mudm.icefield.model.player.Character;
+import hu.mudm.icefield.view.GUI_skeleton;
+
+public class BuildTentAction extends Action {
+    public BuildTentAction(Character character) {
+        super(character);
+    }
+
+    @Override
+    public void performAction() {
+        GUI_skeleton.printlnWithTabs(this.getClass(),"performAction()");
+
+        GUI_skeleton.raiseTabCnt();
+        IceFloat ice = ch.getIceFloat();
+        GUI_skeleton.decreaseTabCnt();
+
+        GUI_skeleton.raiseTabCnt();
+        ice.buildTent();
+        GUI_skeleton.decreaseTabCnt();
+    }
+}
