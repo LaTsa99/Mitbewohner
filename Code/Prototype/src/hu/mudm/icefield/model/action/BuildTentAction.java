5a40250f719d414ff588396d30de2ba6a2cea984
Tamás Visy <tamas.visy@gmail.com>
2020-04-26 21:31:40 +0200

    Tent, BuildTentAction fix

--- Code/Prototype/src/hu/mudm/icefield/model/action/BuildTentAction.java
+++ Code/Prototype/src/hu/mudm/icefield/model/action/BuildTentAction.java
@@ -1,7 +1,5 @@
 package hu.mudm.icefield.model.action;
 
-import hu.mudm.icefield.model.field.IceFloat;
-import hu.mudm.icefield.model.item.Tent;
 import hu.mudm.icefield.model.player.Character;
 
 public class BuildTentAction extends Action {
@@ -11,10 +9,7 @@ public class BuildTentAction extends Action {
 
     @Override
     public void performAction() {
-        IceFloat ice = ch.getIceFloat();
-        ice.buildTent();
-
-        Tent t = new Tent();
-        ch.removeItem(t);
+        ch.BuildTent();
+        ch.getPosition().buildTent();
     }
 }
