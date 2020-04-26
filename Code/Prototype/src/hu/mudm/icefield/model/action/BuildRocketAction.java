cda76d248c164936b691e69caa8724dbf5c03a5f
Marci <golejmarci@gmail.com>
2020-04-26 18:34:11 +0200

    szerintem most mar talan mukodokepes a BuildRocketAction ehhez Controller -ben a checkWinningStatus visszateresi erteket atraktam booleanra és a game ben csinaltam egy getControllert

--- Code/Prototype/src/hu/mudm/icefield/model/action/BuildRocketAction.java
+++ Code/Prototype/src/hu/mudm/icefield/model/action/BuildRocketAction.java
@@ -6,21 +6,17 @@ import hu.mudm.icefield.view.GUI_skeleton;
 
 public class BuildRocketAction extends Action {
 
-    private static int pickedUp = 0;
     Game g;
 
     public BuildRocketAction(Character character) {
         super(character);
     }
 
-    public static int pickedUpParts(){
-        return pickedUp;
-    }
-
     @Override
     public void performAction() {
         GUI_skeleton.printlnWithTabs(this.getClass(),"performAction()");
-        if (pickedUp==3) {
+        g = Game.getInstance();
+        if (g.GetController().checkWinningStatus()) {
             g.win();
         }
     }
