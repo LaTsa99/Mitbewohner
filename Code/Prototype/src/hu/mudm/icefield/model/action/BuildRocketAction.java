affdbbbbd74bd395de0369baa45c8e1eb87fe219
Tamás Visy <tamas.visy@gmail.com>
2020-04-26 21:18:02 +0200

    BuildRocketAction egyéb részei javítva

--- Code/Prototype/src/hu/mudm/icefield/model/action/BuildRocketAction.java
+++ Code/Prototype/src/hu/mudm/icefield/model/action/BuildRocketAction.java
@@ -1,20 +1,11 @@
 package hu.mudm.icefield.model.action;
 
-import hu.mudm.icefield.Game;
 import hu.mudm.icefield.model.Controller;
 import hu.mudm.icefield.model.player.Character;
 
 public class BuildRocketAction extends Action {
-
-    private static int pickedUp = 0;
-    Game g;
-
     public BuildRocketAction(Character character) {
         super(character);
-    }
-
-    public static int pickedUpParts(){
-        return pickedUp;
     }
 
     @Override
