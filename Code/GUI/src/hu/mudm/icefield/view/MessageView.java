1437f8ee81f6bcd551a6d3e65be1464e2925c595
Tamás Visy <tamas.visy@gmail.com>
2020-05-19 17:13:11 +0200

    MessageView Update-je

--- Code/GUI/src/hu/mudm/icefield/view/MessageView.java
+++ Code/GUI/src/hu/mudm/icefield/view/MessageView.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 public class MessageView extends MVCView {
 
     private JLabel label;
+    private Boolean locktext = false;
 
     public MessageView(MVCModell modell, MenuView mv) {
         super(modell);
@@ -14,9 +15,19 @@ public class MessageView extends MVCView {
     @Override
     public void update() {
 
+        if((Controller)modell.getIsWon())
+        {
+            label.setText("You won!");
+            locktext = true;
+        }
+        if((Controller)modell.getIsLost())
+        {
+            label.setText("You lost!");
+            locktext = true;
+        }
     }
 
     public void setText(String s){
-        label.setText(s);
+        if(!locktext) label.setText(s);
     }
 }
